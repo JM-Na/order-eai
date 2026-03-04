e442bcd63a2409ed4e33a0f7c118806adf6f3801
Jongmyeong <myung0484@naver.com>
2026-03-04 16:36:12 +0900

    build: Initial commit
    최초 스프링 프로젝트 생성.

---
+++ src/main/java/com/jmna/order_eai/OrderEaiApplication.java
@@ -0,0 +1,13 @@
+package com.jmna.order_eai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEaiApplication.class, args);
+	}
+
+}
