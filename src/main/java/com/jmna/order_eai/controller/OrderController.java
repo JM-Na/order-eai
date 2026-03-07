28351f2e18fbd40dafcd752d526cca09d92e8543
Jongmyeong <myung0484@naver.com>
2026-03-07 21:41:15 +0900

    style: 불필요한 코드 제거.

--- src/main/java/com/jmna/order_eai/controller/OrderController.java
+++ src/main/java/com/jmna/order_eai/controller/OrderController.java
@@ -1,17 +1,13 @@
 package com.jmna.order_eai.controller;
 
 import com.jmna.order_eai.dto.*;
-import com.jmna.order_eai.service.FtpService;
 import com.jmna.order_eai.service.OrderService;
-import com.jmna.order_eai.service.ReceiptFileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RequestMapping(value = "/api")
@@ -20,8 +16,6 @@ import java.util.Map;
 public class OrderController {
 
     private final OrderService orderService;
-    private final ReceiptFileService receiptFileService;
-    private final FtpService ftpService;
 
     @ResponseBody
     @PostMapping(value = "/receive", consumes = MediaType.APPLICATION_XML_VALUE)
--- src/main/java/com/jmna/order_eai/dto/ValidateResult.java
+++ src/main/java/com/jmna/order_eai/dto/ValidateResult.java
@@ -2,7 +2,6 @@ package com.jmna.order_eai.dto;
 
 import lombok.Data;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
